7df2f39f2f72a074d48dc265e783e619776a8ee0
paulosedassari <pssantos.ph200@gmail.com>
2024-06-26 15:57:08 -0300

    feat: Primeiro commit da aplicação; config .gitignore

---
+++ src/main/java/br/com/cashme/CashmeServiceApplication.java
@@ -0,0 +1,13 @@
+package br.com.cashme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashmeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CashmeServiceApplication.class, args);
+	}
+
+}
