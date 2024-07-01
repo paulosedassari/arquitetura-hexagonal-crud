c854cf83dead2e63c89fd0b6d6f5657b86bf0213
paulosedassari <pssantos.ph200@gmail.com>
2024-07-01 14:10:58 -0300

    refactor: add validadores na entity.

--- src/main/java/br/com/cashme/adapter/outbound/entity/SimulacaoEntity.java
+++ src/main/java/br/com/cashme/adapter/outbound/entity/SimulacaoEntity.java
@@ -1,12 +1,12 @@
 package br.com.cashme.adapter.outbound.entity;
 
-import br.com.cashme.domain.model.Cliente;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
@@ -29,15 +29,19 @@ public class SimulacaoEntity {
     @Column(name = "data_hora")
     private LocalDateTime dataHora;
 
+    @NotBlank
     @Column(name = "valor_solicitado")
     private BigDecimal valorSolicitado;
 
+    @NotBlank
     @Column(name = "valor_garantia")
     private BigDecimal valorGarantia;
 
+    @NotBlank
     @Column(name = "qtd_meses_financiamento")
     private int qtdMesesFinanciamento;
 
+    @NotBlank
     @Column(name = "taxa_juros_mensal")
     private BigDecimal taxaJurosMensal;
 }
